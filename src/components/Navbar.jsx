17f075abd9a5419abc68df096f22be0efd55a117
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-01 12:10:30 +0300

    created a navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "../components/FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "../state/themeSlice";
+import { AppBar, InputBase, Toolbar, useTheme, IconButton } from "@mui/material";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const dispatch = useDispatch();
+  const theme = useTheme();
 
-export default Navbar
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* Left side */}
+        <FlexBetween>
+          <IconButton onClick={() => console.log("open/close sidebar")}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* rigth side */}
+        <FlexBetween gap='1.5rem'>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === 'dark' ? (
+              <DarkModeOutlined sx={{ fontSize: '25px' }} />
+            ):(
+              <LightModeOutlined sx={{ fontSize: '25px' }} />
+            )}
+
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined />
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
