fd7f1be4d114672bf199d69cb5ef04d759f5b8a4
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-09 23:29:48 +0300

    added a login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,99 @@
+import { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { setCredentials } from "../features/auth/authSlice";
+import { useLoginMutation } from "../features/auth/authApiSlice";
+
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [email, setEmail] = useState("admin1@test.com");
+  const [pwd, setPwd] = useState("123456");
+  const [errMsg, setErrMsg] = useState("");
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [email, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      
+      const info = JSON.stringify({ email, password: pwd });
+      const userData = await login(info).unwrap();
+      console.log(userData);
+      dispatch(setCredentials({ ...userData, email }));
+      setEmail("");
+      setPwd("");
+      navigate("/dashboard");
+    } catch (err) {
+      if (!err?.originalStatus) {
+        // isLoading: true until timeout occurs
+        setErrMsg("No Server Response");
+      } else if (err.originalStatus === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.originalStatus === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  const handleEmailInput = (e) => setEmail("admin1@test.com");
+
+  const handlePwdInput = (e) => setPwd("123456");
+
+  const content = isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <section className="login">
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+
+      <h1>Employee Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          ref={userRef}
+          value={email}
+          onChange={handleEmailInput}
+          autoComplete="off"
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={handlePwdInput}
+          value={pwd}
+          required
+        />
+        <button>Sign In</button>
+      </form>
+    </section>
+  );
+
+  return content;
+};
+export default Login;
