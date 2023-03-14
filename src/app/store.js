b13a1ccf4eac0b525c6d7f7867885c8de2faa2a9
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-15 00:03:23 +0300

    added redux persist to persist on user login

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit"
-import themeReducer from '../features/theme/themeSlice'
+// import themeReducer from '../features/theme/themeSlice'
 import { apiSlice } from "./api/apiSlice"
-import authReducer from '../features/auth/authSlice'
+// import authReducer from '../features/auth/authSlice'
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./rootReducer";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    blacklist:[rootReducer.theme]
+  };
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        theme: themeReducer,
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer
-    },
+    reducer: persistedReducer,
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
 })
+
+export const persistor = persistStore(store);
