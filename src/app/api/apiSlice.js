f714fe7b0499450858d5e7e606f028cf441ef702
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-17 23:56:22 +0300

    added function to get csrftoken from headers

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,45 +1,52 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { setCredentials, logOut } from '../../features/auth/authSlice'
+import { setCredentials, setCSRFToken, logOut } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://127.0.0.1:8000/api',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
-        const csrfToken  = getState().auth.csrftoken
-        if (token && csrfToken) {
+        if (token) {
             headers.set("Authorization", `Bearer ${token}`)
-            headers.set('csrftoken', csrfToken)
         }
-        // headers.set('Content-Type', 'application/json');
-
         return headers
-    }
+    },
+    fetchFn: async (...args) => {
+        const response = await fetch(...args);
+        const csrftoken = response.headers.get('csrftoken');
+        if (csrftoken) {
+            // Update the authentication slice with the CSRF token
+            //   store.dispatch(authSlice.actions.setCSRFToken(csrftoken));
+            store.dispatch(setCSRFToken(csrftoken));
+
+        }
+        return response;
+    },
 })
 
-const baseQueryWithReauth = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions)
+// const baseQueryWithReauth = async (args, api, extraOptions) => {
+//     let result = await baseQuery(args, api, extraOptions)
 
-    if (result?.error?.originalStatus === 403) {
-        console.log('sending refresh token')
-        // send refresh token to get new access token 
-        const refreshResult = await baseQuery('/auth/refresh-token', api, extraOptions)
-        console.log(refreshResult)
-        if (refreshResult?.data) {
-            const user = api.getState().auth.user
-            // store the new token 
-            api.dispatch(setCredentials({ ...refreshResult.data, user }))
-            // retry the original query with new access token 
-            result = await baseQuery(args, api, extraOptions)
-        } else {
-            api.dispatch(logOut())
-        }
-    }
+//     if (result?.error?.originalStatus === 401) {
+//         console.log('sending refresh token')
+//         // send refresh token to get new access token 
+//         const refreshResult = await baseQuery('/auth/refresh-token', api, extraOptions)
+//         console.log(refreshResult)
+//         if (refreshResult?.data) {
+//             const user = api.getState().auth.user
+//             // store the new token 
+//             api.dispatch(setCredentials({ ...refreshResult.data, user }))
+//             // retry the original query with new access token 
+//             result = await baseQuery(args, api, extraOptions)
+//         } else {
+//             api.dispatch(logOut())
+//         }
+//     }
 
-    return result
-}
+//     return result
+// }
 
 export const apiSlice = createApi({
-    baseQuery: baseQueryWithReauth,
+    baseQuery,
     endpoints: builder => ({})
 })
