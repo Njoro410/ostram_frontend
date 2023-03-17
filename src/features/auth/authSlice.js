aa6f01a5bde5dbc8a0fee36c8145ec0f0e2eea8b
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-17 23:56:55 +0300

    added a reducer to set csrf token

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -2,13 +2,17 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const authSlice = createSlice({
     name: 'auth',
-    initialState: { user: null, token: null, refresh: null },
+    initialState: { user: null, token: null, refresh: null, csrftoken:null },
     reducers: {
         setCredentials: (state, action) => {
             const { user, accessToken, refreshToken } = action.payload
             state.user = user
             state.token = accessToken
             state.refresh = refreshToken
+        },
+        setCSRFToken :(state, action) => {
+            const { csrftoken } = action.payload
+            state.csrftoken = csrftoken
         },
         logOut: (state, action) => {
             state.user = null
@@ -18,9 +22,10 @@ const authSlice = createSlice({
     },
 })
 
-export const { setCredentials, logOut } = authSlice.actions
+export const { setCredentials, setCSRFToken, logOut } = authSlice.actions
 
 export default authSlice.reducer
 
 export const selectCurrentUser = (state) => state.auth.user
 export const selectCurrentToken = (state) => state.auth.token
+export const selectCSRFToken = (state) => state.auth.csrftoken;
