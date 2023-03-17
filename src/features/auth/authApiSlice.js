54754acd7996878882437cbd55c53ece21c7c402
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-17 23:55:05 +0300

    added function to get new refresh token by sending csrf token

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -1,4 +1,8 @@
 import { apiSlice } from "../../app/api/apiSlice";
+import { setCSRFToken } from '../../features/auth/authSlice'
+import { useCSRFToken } from "../hooks/useCSRFToken";
+
+const csrfToken = useCSRFToken();
 
 export const authApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
@@ -12,7 +16,40 @@ export const authApiSlice = apiSlice.injectEndpoints({
                   },
             })
         }),
-    })
+        onfulfilled: (response) => {
+            const csrftoken = response.headers.get('csrftoken');
+            if (csrftoken) {
+      
+              store.dispatch(setCSRFToken(csrftoken));
+            }
+          },
+    }),
+    refreshTokens: builder.mutation({
+        query: () => ({
+          url: '/auth/refresh-tokens',
+          method: 'POST',
+          headers: {
+            'X-CSRFToken': csrfToken,
+          },
+        }),
+      }),
+    // }),
+    onError: (error) => {
+      if (error.status === 403) {
+        // Refresh the CSRF token
+        fetch('/auth/login/', { credentials: 'include' })
+          .then((response) => {
+            const csrftoken = response.headers.get('csrftoken');
+            if (csrftoken) {
+              // Update the authentication slice with the new CSRF token
+              store.dispatch(setCSRFToken(csrftoken));
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
 })
 
 
