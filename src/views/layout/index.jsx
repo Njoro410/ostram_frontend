904a49d91321eeeb7849e2f40b58455c8cbbdba1
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-21 23:59:30 +0300

    added drawer handling function

--- src/views/layout/index.jsx
+++ src/views/layout/index.jsx
@@ -2,14 +2,42 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
+import { useState } from "react";
+import Appbar from "../../components/Appbar";
+import MyAppBar from "../../components/Appbar";
+import DrawerComponent from "../../components/Drawer";
+import AppBarComponent from "../../components/Appbar";
+
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [open, setOpen] = useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Box display={isNonMobile ? "flex" : "flex"} width="100%" height="100%">
-      <Sidebar />
+      {/* <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      /> */}
+      <DrawerComponent open={open} handleDrawerClose={handleDrawerClose}        />
       <Box flexGrow={1}>
-        <Navbar />
+        {/* <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        /> */}
+        {/* <Appbar /> */}
+        <AppBarComponent open={open} handleDrawerOpen={handleDrawerOpen} />
         <Outlet />
       </Box>
     </Box>
