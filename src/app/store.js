ed682e2f9e5c60575dfb41c121aed650ac5a3bbf
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-29 23:54:54 +0300

    imported redux persist modules to be ignored

--- src/app/store.js
+++ src/app/store.js
@@ -2,22 +2,35 @@ import { configureStore } from "@reduxjs/toolkit"
 // import themeReducer from '../features/theme/themeSlice'
 import { apiSlice } from "./api/apiSlice"
 // import authReducer from '../features/auth/authSlice'
-import { persistStore, persistReducer } from "redux-persist";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./rootReducer";
 
 const persistConfig = {
     key: "root",
     storage,
-    blacklist:[rootReducer.theme]
-  };
+    blacklist: [rootReducer.theme]
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
     middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(apiSlice.middleware),
     devTools: true
 })
 
