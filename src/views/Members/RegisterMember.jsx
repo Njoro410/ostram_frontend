df79d746cac14147bcf8c4e1f93a826094d1f539
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-31 23:54:43 +0300

    added a member registration component

---
+++ src/views/Members/RegisterMember.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Header from '../../components/Header'
+
+const RegisterMember = () => {
+  return (
+    <Box m="5.5rem 2.5rem">
+    <Header title="MEMBER REGISTRATION" subtitle="Register new members by filling their details" />
+  </Box>
+  )
+}
+
+export default RegisterMember
