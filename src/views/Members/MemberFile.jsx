e7e9e90feceaca22a4d2987beaee6a6f1b94594d
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-31 23:56:46 +0300

    added new components

---
+++ src/views/Members/MemberFile.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import React from "react";
+import Header from "../../components/Header";
+
+const MemberFile = () => {
+  return (
+    <Box m="5.5rem 2.5rem">
+      <Header title="MEMBER LIST" subtitle="A data grid of all members" />
+    </Box>
+  );
+};
+
+export default MemberFile;
