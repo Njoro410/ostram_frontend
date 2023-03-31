30e5fa53bf02cdcb8e4c9ca4fa85dad5eafffa3c
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-31 23:52:35 +0300

    imported new components

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ import Dashboard from "./views/dashboard";
 import Layout from "./views/layout";
 import Login from "./pages/Login";
 import RequireAuth from "./features/auth/RequireAuth";
+import MemberList from "./views/Members/MemberList";
+import RegisterMember from "./views/Members/RegisterMember";
+import MemberFile from "./views/Members/MemberFile";
+import CrbInfo from "./views/Members/CrbInfo";
+import SavingsAccount from "./views/Accounts/SavingsAccount";
+import DepositsAccount from "./views/Accounts/DepositsAccount";
+import Contributions from "./views/Accounts/Contributions";
+import SignIn from "./pages/SignIn";
+
 
 function App() {
   const mode = useSelector((state) => state.theme.mode);
@@ -20,13 +29,23 @@ function App() {
           <Routes>
             {/* public routes */}
 
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<SignIn />} />
 
             {/* protected routes */}
 
             <Route element={<RequireAuth />}>
               <Route element={<Layout />}>
                 <Route path="/dashboard" element={<Dashboard />} />
+                {/* members links */}
+                <Route path="/view members" element={<MemberList />} />
+                <Route path="/member registration" element={<RegisterMember />} />
+                <Route path="/member file" element={<MemberFile />} />
+                <Route path="/crb information" element={<CrbInfo />} />
+                  {/* accounts links */}
+                <Route path="/savings account" element={<SavingsAccount />} />
+                <Route path="/deposits account" element={<DepositsAccount />} />
+                <Route path="/add contributions" element={<Contributions />} />
+
               </Route>
             </Route>
           </Routes>
