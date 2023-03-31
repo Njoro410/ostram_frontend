3dd0524c91cb9f04637e0b5afe8a761fad985539
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-31 23:55:49 +0300

    added a yup validation schema

---
+++ src/utils/validationSchema.js
@@ -0,0 +1,9 @@
+import * as Yup from "yup";
+
+export const loginValidationSchema = Yup.object().shape({
+    email: Yup.string()
+      .required('Email is required')
+      .email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required'),
+  });
