76d6dc96b86c696e4fe4aa9ee56cf187d0599615
njoro410 <brian.njoroge@student.moringaschool.com>
2023-04-01 23:37:10 +0300

    passed logged in user name to header component

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,9 +1,157 @@
-import React from 'react'
+import React from "react";
+import FlexBetween from "../../components/FlexBetween";
+import Header from "../../components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import StatBox from "../../components/StatBox";
+import { Scrollbars } from "react-custom-scrollbars";
+import { useGetUserQuery } from "../../features/users/usersApiSlice";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const theme = useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
 
-export default Dashboard
+  const {
+    data,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUserQuery();
+  // console.log(data.results)
+ 
+
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="DASHBOARD" subtitle={`Welcome ${data?.results.fullname}`} />
+
+        {/* <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+              mt:"25px"
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box> */}
+      </FlexBetween>
+
+      <Box
+        mt="20px"
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="160px"
+        gap="20px"
+        sx={{
+          "& > div": { gridColumn: isNonMediumScreens ? undefined : "span 12" },
+        }}
+      >
+        {/* ROW 1 */}
+        <StatBox
+          title="New Members"
+
+          increase="+14%"
+          description="Since last month"
+          icon={
+            <Email
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        />
+        <StatBox
+          title="Total loans"
+     
+          increase="+21%"
+          description="Given this month"
+          icon={
+            <PointOfSale
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        />
+
+        <StatBox
+          title="Total Savings"
+        
+          increase="+5%"
+          description="Since last month"
+          icon={
+            <PersonAdd
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        />
+        <StatBox
+          title="Total Deposits"
+         
+          increase="+43%"
+          description="Since last month"
+          icon={
+            <Traffic
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        />
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={theme.palette.background.alt}
+          p="1rem"
+          borderRadius="0.55rem"
+        >
+          <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+            weather forecast
+          </Typography>
+        </Box>
+
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={theme.palette.background.alt}
+          p="1rem"
+          borderRadius="0.55rem"
+        >
+          <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+            some graph
+          </Typography>
+        </Box>
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 1"
+          backgroundColor={theme.palette.background.alt}
+          p="1.5rem"
+          borderRadius="0.55rem"
+        >
+          <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+            more stuff
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
