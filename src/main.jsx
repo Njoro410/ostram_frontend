cb439126fe14b725ac07ed2eec678d7379347e21
njoro410 <brian.njoroge@student.moringaschool.com>
2023-04-03 21:08:36 +0300

    temporarily commented strict mode for debugging purposes

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,12 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./app/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
