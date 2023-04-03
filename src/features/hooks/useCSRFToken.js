bb096f4d1e31548ca5def6a2e223ec15fd467df6
njoro410 <brian.njoroge@student.moringaschool.com>
2023-04-03 21:10:08 +0300

    removed these components

--- src/features/hooks/useCSRFToken.js
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux';
-import { selectCSRFToken } from '../auth/authSlice';
-
-export const useCSRFToken = () => {
-  const csrfToken = useSelector(selectCSRFToken);
-  return csrfToken;
-};
