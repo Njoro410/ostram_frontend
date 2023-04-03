9de01ff8843eb51ea64044f4cb7baa8298466992
njoro410 <brian.njoroge@student.moringaschool.com>
2023-04-03 21:00:23 +0300

    added a MemberList component to display a data grid of memebers

---
+++ src/views/Members/MemberList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
+
+const Memberlist = () => {
+  return (
+    <Box m="5.5rem 2.5rem">
+      <Header title="MEMBER LIST" subtitle="A data grid of all members" />
+    </Box>
+  );
+};
+
+export default Memberlist;
