aac04a61a283a03670c72631764c1a662e2b9938
njoro410 <brian.njoroge@student.moringaschool.com>
2023-05-23 20:31:58 +0300

    chart:Added a spline area chart

---
+++ src/charts/SplineArea.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+import { useSelector } from "react-redux";
+
+const SplineArea = () => {
+  const tmode = useSelector((state) => state.theme.mode);
+  const options = {
+    chart: {
+      height: 350,
+      type: "area",
+      background: 'transparent',
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      type: "datetime",
+      categories: [
+        "2018-09-19T00:00:00.000Z",
+        "2018-09-19T01:30:00.000Z",
+        "2018-09-19T02:30:00.000Z",
+        "2018-09-19T03:30:00.000Z",
+        "2018-09-19T04:30:00.000Z",
+        "2018-09-19T05:30:00.000Z",
+        "2018-09-19T06:30:00.000Z",
+      ],
+    },
+    tooltip: {
+      x: {
+        format: "dd/MM/yy HH:mm",
+      },
+    },
+    grid: {
+      show: true,
+      borderColor: "#90A4AE",
+      strokeDashArray: 0,
+      position: "back",
+      xaxis: {
+        lines: {
+          show: false,
+        },
+      },
+      yaxis: {
+        show: false,
+        lines: {
+          show: false,
+        },
+      },
+      row: {
+        colors: undefined,
+        opacity: 0.5,
+      },
+      column: {
+        colors: undefined,
+        opacity: 0.5,
+      },
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      },
+    },
+    yaxis :{
+        show: true,
+        labels: {
+            show: false
+        }
+    },
+    theme: {
+      mode: tmode,
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+      ],
+    },
+
+    series: [
+      {
+        name: "series1",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+      {
+        name: "series2",
+        data: [11, 32, 45, 32, 34, 52, 41],
+      },
+    ],
+  };
+
+  return (
+    <ReactApexChart
+      options={options}
+      series={options.series}
+      type="area"
+      height={250}
+    />
+  );
+};
+
+export default SplineArea;
