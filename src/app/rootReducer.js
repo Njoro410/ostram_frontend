e18f3d4b350abf3c3831ef3c9b7b790f11bb9b40
njoro410 <brian.njoroge@student.moringaschool.com>
2023-05-30 22:47:12 +0300

    registered weather api to root reducer

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -2,11 +2,13 @@ import themeReducer from '../features/theme/themeSlice'
 import { apiSlice } from "./api/apiSlice"
 import authReducer from '../features/auth/authSlice'
 import { combineReducers } from '@reduxjs/toolkit';
+import weatherApi  from './api/weatherSlice';
 
 const rootReducer = combineReducers({
-    theme: themeReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authReducer
-  });
-  
-  export default rootReducer;
+  theme: themeReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  [weatherApi.reducerPath]: weatherApi.reducer,
+  auth: authReducer
+});
+
+export default rootReducer;
