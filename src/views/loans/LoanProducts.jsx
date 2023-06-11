13ad7ff7719091d61ec426a9e63d810d17a24eb9
njoro410 <brian.njoroge@student.moringaschool.com>
2023-06-11 05:25:22 +0300

    moved card components to separate file

--- src/views/loans/LoanProducts.jsx
+++ src/views/loans/LoanProducts.jsx
@@ -1,59 +1,38 @@
-import {
-  Avatar,
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  Chip,
-  Divider,
-  IconButton,
-  Typography,
-  useMediaQuery,
-} from "@mui/material";
-import React from "react";
+import { Box, Button, useMediaQuery } from "@mui/material";
+import React, { useState } from "react";
 import FlexBetween from "../../components/FlexBetween";
 import Header from "../../components/Header";
 import { useTheme } from "@emotion/react";
 import GroupedStackedColumn from "../../charts/GroupedStackedColumn";
 import PieChart from "../../charts/PieChart";
-import styled from "@emotion/styled";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-
-const GlassCard = styled(Card)`
-  background-color: rgba(87, 86, 86, 0.25);
-  backdrop-filter: blur(10px);
-  border-radius: 8px;
-  padding: 10px;
-`;
-
-const StyledCardHeader = styled(CardHeader)`
-  .MuiCardHeader-title {
-    font-size: 18px;
-    color: #f3f3f3;
-    font-weight: bold;
-    /* text-align: center; */
-  }
-`;
-
-const StyledChip = styled(Chip)`
-  font-weight: bold;
-  font-size:0.75rem;
-`;
+import { useGetAllLoanTypesQuery } from "../../services/loans/loanSlices";
+import LoanProductCard from "../../components/LoanComponents/LoanProductCard";
+import AddLoanProductModal from "../../components/LoanComponents/AddLoanProductModal";
 
 const LoanProducts = () => {
   const theme = useTheme();
   const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
-  const rate = 1.2;
+  const [openModal, setOpenModal] = useState(false);
+  const handleModalOpen = () => setOpenModal(true);
+  const handleModalClose = () => setOpenModal(false);
+
+  const {
+    data: loan_types,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetAllLoanTypesQuery();
 
   return (
     <Box mt="2rem">
+      <AddLoanProductModal open={openModal} onClose={handleModalClose}/>
       <FlexBetween>
         <Header title="LOAN PRODUCTS" subtitle="Details about loan products" />
 
         <Box>
           <Button
+            onClick={handleModalOpen}
             sx={{
               backgroundColor: theme.palette.secondary.light,
               color: theme.palette.background.alt,
@@ -76,9 +55,8 @@ const LoanProducts = () => {
         display="grid"
         gridTemplateColumns="repeat(12, 1fr)"
         gridAutoRows="160px"
-        // backgroundColor={theme.palette.background.alt}
         borderRadius="0.55rem"
-        // height='100vh'
+        // height="100vh"
         p="1rem"
         gap="20px"
         sx={{
@@ -91,9 +69,11 @@ const LoanProducts = () => {
           height="25rem"
           backgroundColor={theme.palette.background.alt}
           borderRadius="0.55rem"
-          // border="1px solid"
-          // borderColor={theme.palette.secondary[800]}
           paddingY={2}
+          sx={{
+            border: (theme) => `1px solid ${theme.palette.divider}`,
+            borderRadius: 1,
+          }}
         >
           <GroupedStackedColumn />
         </Box>
@@ -104,56 +84,28 @@ const LoanProducts = () => {
           height="25rem"
           backgroundColor={theme.palette.background.alt}
           borderRadius="0.55rem"
-          // border="1px solid"
-          // borderColor={theme.palette.secondary[800]}
           paddingY={2}
+          sx={{
+            border: (theme) => `1px solid ${theme.palette.divider}`,
+            borderRadius: 1,
+          }}
         >
           <PieChart />
         </Box>
-
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={theme.palette.background.alt}
-          borderRadius="0.55rem"
-          // border="1px solid"
-          // borderColor={theme.palette.secondary[800]}
-          p={2}
-          mt="3.5rem"
-        >
-          <GlassCard variant="outlined" sx={{ height: "100%" }}>
-            <FlexBetween>
-              <StyledCardHeader
-                title="Wezesha Loan"
-                titleTypographyProps={{
-                  component: "h1",
-                }}
-              />
-
-              <StyledChip label={`Rate | ${rate}%`} variant="filled" color="primary" />
-            </FlexBetween>
-            <Divider variant="middle" />
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color={theme.palette.secondary.light}
-                gutterBottom
-                variant="subtitle2"
-              >
-                Description
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Edit</Button>
-              <Button size="small">Delete</Button>
-            </CardActions>
-          </GlassCard>
-        </Box>
+        {loan_types?.results.map((type) => (
+          <Box
+            gridColumn="span 4"
+            gridRow="span 2"
+            // gridRow={expanded ? "span 3" : "span 2"}
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="0.55rem"
+            p={2}
+            mt="3.5rem"
+            height="fit-content"
+          >
+            <LoanProductCard key={type.id} type={type} />
+          </Box>
+        ))}
       </Box>
     </Box>
   );
